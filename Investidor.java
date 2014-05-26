5b19d4b46109deaf553cdbecd4381f23573448e8
Aluno <me@me.com>
2014-05-26 11:34:01 -0300

    Ivestidor Imprlemtado

---
+++ Investidor.java
@@ -0,0 +1,13 @@
+public class Investidor extends Cliente {
+
+	private float capitalInvestido;
+
+	public float getCapitalInvestido() {
+		return capitalInvestido;
+	}
+
+	public void setCapitalInvestido(float capitalInvestido) {
+		this.capitalInvestido = capitalInvestido;
+	}
+	
+}
